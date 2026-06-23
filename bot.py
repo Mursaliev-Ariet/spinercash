4b0904d1ed0c9fc196500eb6cff03b9005be9c48
Mursaliev-Ariet <d98631996@gmail.com>
2026-06-23 11:42:23 +0600

    first commit

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 
 import os
-TOKEN = "8842127375:AAGeEiDRPwYLrC49EVlhDQ4uhnX8q12j5hA"
+TOKEN = os.getenv("TOKEN")
 
 
 REWARDS = [
